6b647a0e549cab3dd3a97170351bd877461c4095
Rafaela Keller <rafaela.keller.rk@gmail.com>
2025-05-25 22:56:48 -0300

    salvando no git hub

---
+++ Models/Produto.cs
@@ -0,0 +1,9 @@
+﻿namespace andromeda_back.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsTimed { get; set; }
+    }
+}
---
+++ Services/ProdutosService.cs
@@ -0,0 +1,47 @@
+﻿using andromeda_back.Models;
+using System.Xml.Linq;
+
+namespace andromeda_back.Services
+{
+    public class ProdutosService
+    {
+        static List<Produto> Produtos { get; }
+        static int nextId = 3;
+        static ProdutoService()
+        {
+            Produtos = new List<Produto>
+        {
+            new Produto { Id = 1, Name = "Classic Italian", IsTimed = false },
+            new Produto { Id = 2, Name = "Veggie", IsTimed = true }
+        };
+        }
+
+        public static List<Produto> GetAll() => Produtos;
+
+        public static Produto? Get(int id) => Produtos.FirstOrDefault(p => p.Id == id);
+
+        public static void Add(Produto produto)
+        {
+            produto.Id = nextId++;
+            Produtos.Add(produto);
+        }
+
+        public static void Delete(int id)
+        {
+            var produto = Get(id);
+            if (produto is null)
+                return;
+
+            Produtos.Remove(produto);
+        }
+
+        public static void Update(Produto produto)
+        {
+            var index = Produtos.FindIndex(p => p.Id == produto.Id);
+            if (index == -1)
+                return;
+
+            Produtos[index] = produto;
+        }
+    }
+}
